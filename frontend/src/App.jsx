03c5158f5ddcfc97605249146cc00cddb9c61692
omar ayadi <omarayadi013@gmail.com>
2025-09-13 23:44:50 +0100

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,18 +7,6 @@ import ResumeUpload from "../views/components/ResumeUpload";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <nav className="p-4 bg-gray-200 flex gap-4">
-        <Link to="/login">Login</Link>
-        <Link to="/signup">Sign Up</Link>
-        <Link to="/uploadcv">Upload CV</Link>
-      </nav>
-
-      <Routes>
-        <Route path="/login" element={<LogIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/uploadcv" element={<ResumeUpload />} />
-      </Routes>
-    </BrowserRouter>
+    <LogIn/>
   );
 }
